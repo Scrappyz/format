437961ebcb0528457e3271bc62cc0e4a4d95577d
Scrappyz <ghostmic3000@gmail.com>
2024-08-03 22:18:29 +0800

    changed private functions

--- include/format.hpp
+++ include/format.hpp
@@ -4,120 +4,172 @@
 #include <vector>
 
 namespace format {
-    std::string table(const std::vector<std::vector<std::string>>& matrix, int space = 1) 
+
+    namespace _private_ {
+        std::vector<std::vector<std::string>> cleanTableValues(const std::vector<std::vector<std::string>>& table_values);
+        std::vector<int> maxColumnWidths(const std::vector<std::string>& headers, const std::vector<std::vector<std::string>>& table_values);
+        std::vector<int> maxColumnWidths(const std::vector<std::vector<std::string>>& table_values);
+    }
+
+    inline std::string table(const std::vector<std::string>& headers, const std::vector<std::vector<std::string>>& table_values,
+                             const std::string& border_between, const std::string& border_left, const std::string& border_right,
+                             const std::string& left_margin)
     {
-        if(space < 0) {
-            space = 0;
-        }
-
-        /*
-            Add new rows based on the max number of endlines found in the row
-            Eg:
-            {
-                {"Apple", "Banana\nNana\nBan", "Cherry\nHenry"},
-                {"Grapes", "Orange", "Pineapple"}
-            }
-            to
-            {
-                {"", "", ""},
-                {"", "", ""},
-                {"", "", ""},
-                {"", "", ""}
-            }
-
-            In this case, the max number of '\n' in the first row is 2. 
-            So we add 2 more rows.
-        */
-        std::vector<std::vector<std::string>> cleaned_matrix(matrix.size(), std::vector<std::string>(matrix[0].size(), ""));
-        for(int i = 0; i < matrix.size(); i++) {
-            int max_endlines = 0;
-            for(int j = 0; j < matrix[i].size(); j++) {
-                int endlines = 0;
-
-                for(int k = 0; k < matrix[i][j].size(); k++) {
-                    char ch = matrix[i][j][k];
-
-                    if(ch == '\n') {
-                        endlines++;
-                    }
-                }
-
-                max_endlines = std::max(max_endlines, endlines);
-            }
-
-            for(int j = 0; j < max_endlines; j++) {
-                cleaned_matrix.push_back(std::vector<std::string>(matrix[i].size(), ""));
-            }
-        }
-
-        /*
-            Put the strings next to the newline to the next row.
-            Eg:
-            {
-                {"Apple", "Banana\nNana\nBan", "Cherry\nHenry"},
-                {"Grapes", "Orange", "Pineapple"}
-            }
-            to
-            {
-                {"Apple", "Banana", "Cherry"},
-                {"", "Nana", "Henry"},
-                {"", "Ban", ""},
-                {"Grapes", "Orange", "Pineapple"}
-            }
-        */
-        int temp_i = 0;
-        for(int i = 0; i < matrix.size(); i++) {
-            int max_endlines = 0;
-
-            for(int j = 0; j < matrix[i].size(); j++) {
-                int endlines = 0;
-
-                for(int k = 0; k < matrix[i][j].size(); k++) {
-                    char ch = matrix[i][j][k];
-
-                    if(ch == '\n') {
-                        endlines++;
-                        continue;
-                    }
-
-                    cleaned_matrix[temp_i + endlines][j].push_back(ch);
-                }
-
-                max_endlines = std::max(max_endlines, endlines);
-            }
-
-            temp_i += max_endlines + 1;
-        }
-
-        // Determine the maximum width for all rows
-        int max_width = 0;
-        for(int i = 0; i < cleaned_matrix.size(); i++) {
-            int max_length = 0;
-
-            for(int j = 0; j < cleaned_matrix[i].size(); j++) {
-                if(cleaned_matrix[i][j].size() > max_length) {
-                    max_length = cleaned_matrix[i][j].size();
-                }
-            }
-
-            if(max_length + space > max_width) {
-                max_width = max_length + space;
-            }
-        }
-
         std::string table;
+        std::vector<std::vector<std::string>> cleaned_table_values = _private_::cleanTableValues(table_values);
+
+        if(cleaned_table_values.empty()) {
+            return table;
+        }
+
+        std::vector<int> col_widths = _private_::maxColumnWidths(cleaned_table_values);
 
         // Subtract the max width with the size of the string to get the correct amount of space
-        for(int i = 0; i < cleaned_matrix.size(); i++) {
-            for(int j = 0; j < cleaned_matrix[i].size(); j++) {
-                table += cleaned_matrix[i][j];
-                if(j < cleaned_matrix[i].size()-1) {
-                    table += std::string(max_width - cleaned_matrix[i][j].size(), ' ');
+        for(int i = 0; i < cleaned_table_values.size(); i++) {
+            for(int j = 0; j < cleaned_table_values[i].size(); j++) {
+                std::string value = cleaned_table_values[i][j];
+                table += value;
+                if(j < cleaned_table_values[i].size()-1) {
+                    table += std::string(col_widths[j] - value.size(), ' ') + border_between;
                 }
             }
             table += '\n';
         }
 
         return table;
+    }
+
+    inline std::string table(const std::vector<std::string>& headers, const std::vector<std::vector<std::string>>& table_values,
+                             int space = 1)
+    {
+        if(space < 0) {
+            space = 0;
+        }
+
+        return table(headers, table_values, std::string(space, ' '), "", "", "");
+    }
+
+    inline std::string table(const std::vector<std::vector<std::string>>& table_values, int space = 1) 
+    {
+        return table({}, table_values, space);
+    }
+
+    namespace _private_ {
+        inline std::vector<std::vector<std::string>> cleanTableValues(const std::vector<std::vector<std::string>>& table_values)
+        {
+            /*
+                Add new rows based on the max number of endlines found in the row
+                Eg:
+                {
+                    {"Apple", "Banana\nNana\nBan", "Cherry\nHenry"},
+                    {"Grapes", "Orange", "Pineapple"}
+                }
+                to
+                {
+                    {"", "", ""},
+                    {"", "", ""},
+                    {"", "", ""},
+                    {"", "", ""}
+                }
+
+                In this case, the max number of '\n' in the first row is 2. 
+                So we add 2 more rows.
+            */
+            std::vector<std::vector<std::string>> cleaned_table_values(table_values.size(), std::vector<std::string>(table_values[0].size(), ""));
+            for(int i = 0; i < table_values.size(); i++) {
+                int max_endlines = 0;
+                for(int j = 0; j < table_values[i].size(); j++) {
+                    int endlines = 0;
+
+                    for(int k = 0; k < table_values[i][j].size(); k++) {
+                        char ch = table_values[i][j][k];
+
+                        if(ch == '\n') {
+                            endlines++;
+                        }
+                    }
+
+                    max_endlines = std::max(max_endlines, endlines);
+                }
+
+                for(int j = 0; j < max_endlines; j++) {
+                    cleaned_table_values.push_back(std::vector<std::string>(table_values[i].size(), ""));
+                }
+            }
+
+            /*
+                Put the strings next to the newline to the next row.
+                Eg:
+                {
+                    {"Apple", "Banana\nNana\nBan", "Cherry\nHenry"},
+                    {"Grapes", "Orange", "Pineapple"}
+                }
+                to
+                {
+                    {"Apple", "Banana", "Cherry"},
+                    {"", "Nana", "Henry"},
+                    {"", "Ban", ""},
+                    {"Grapes", "Orange", "Pineapple"}
+                }
+            */
+            int temp_i = 0;
+            for(int i = 0; i < table_values.size(); i++) {
+                int max_endlines = 0;
+
+                for(int j = 0; j < table_values[i].size(); j++) {
+                    int endlines = 0;
+
+                    for(int k = 0; k < table_values[i][j].size(); k++) {
+                        char ch = table_values[i][j][k];
+
+                        if(ch == '\n') {
+                            endlines++;
+                            continue;
+                        }
+
+                        cleaned_table_values[temp_i + endlines][j].push_back(ch);
+                    }
+
+                    max_endlines = std::max(max_endlines, endlines);
+                }
+
+                temp_i += max_endlines + 1;
+            }
+
+            return cleaned_table_values;
+        }
+
+        inline std::vector<int> maxColumnWidths(const std::vector<std::string>& headers, const std::vector<std::vector<std::string>>& table_values)
+        {
+            // Determine the maximum width for all rows
+            std::vector<int> col_widths;
+
+            if(!headers.empty()) {
+                col_widths = std::vector<int>(headers.size(), 0);
+            } else if(!table_values.empty()) {
+                col_widths = std::vector<int>(table_values[0].size(), 0);
+            } else {
+                return col_widths;
+            }
+
+            std::vector<std::vector<std::string>> combined;
+            combined.push_back(headers);
+            combined.insert(combined.end(), table_values.begin(), table_values.end());
+
+            for(int i = 0; i < combined.size(); i++) {
+                for(int j = 0; j < combined[i].size(); j++) {
+                    if(combined[i][j].size() > col_widths[j]) {
+                        col_widths[j] = combined[i][j].size();
+                    }
+                }
+            }
+
+            return col_widths;
+        }
+
+        inline std::vector<int> maxColumnWidths(const std::vector<std::vector<std::string>>& table_values)
+        {
+            return maxColumnWidths({}, table_values);
+        }
     }
 }
--- test/src/format_test.cpp
+++ test/src/format_test.cpp
@@ -1,8 +1,33 @@
 #include <gtest/gtest.h>
+#include "format.hpp"
 
 using namespace std;
 
-TEST(MyTest, play)
+TEST(maxColumnWidths, working)
 {
-    EXPECT_EQ(true, true);
+    std::vector<std::vector<std::string>> strs = {
+        {"Apple", "Banana", "Cherry"},
+        {"Grapes", "Orange", "Pineapple"},
+        {"Strawberry", "Kiwi", "Mango"}
+    };
+
+    std::vector<int> actual = format::_private_::maxColumnWidths(strs);
+    std::vector<int> expected = {10, 6, 9};
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(maxColumnWidths, with_headers)
+{
+    std::vector<std::string> headers = {"Header1", "Header2", "Header3"};
+    std::vector<std::vector<std::string>> strs = {
+        {"Apple", "Banana", "Cherry"},
+        {"Grapes", "Orange", "Pineapple"},
+        {"Strawberry", "Kiwi", "Mango"}
+    };
+
+    std::vector<int> actual = format::_private_::maxColumnWidths(headers, strs);
+    std::vector<int> expected = {10, 7, 9};
+
+    EXPECT_EQ(actual, expected);
 }
